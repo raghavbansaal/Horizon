d098173ad013d4761f3ea04e83a9f556919c52dc
raghavbansaal <88622060+raghavbansaal@users.noreply.github.com>
2026-04-22 01:26:05 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ const publicRoutes = ["/login", "/signup", "/forgot-password"];
 
 export async function middleware(request: NextRequest) {
   if (request.nextUrl.pathname === '/') {
-    return NextResponse.redirect(new URL('/dashboard', request.url));
+    return NextResponse.redirect(new URL('/login', request.url));
   }
   let supabaseResponse = NextResponse.next({
     request,
